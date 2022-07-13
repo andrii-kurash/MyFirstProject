4472997fa32c4a6c02d82bb618b0e94bac70599f
andrii-kurash <kurash.andrii@gmail.com>
2022-07-13 01:00:13 +0000

    the project is completed

--- js/main.js
+++ js/main.js
@@ -13,33 +13,6 @@ $(function(){
     },
   ]
 });
-	$('.reviews__slider').slick({
-		arrows: false,
-		dots: true,
-		slidesToShow: 4,
-  		slidesToScroll: 1,
-  		responsive: [
-    	{
-	      breakpoint: 1141,
-	      settings: {
-	      	slidesToShow: 3,
-     	}
-    },
-    {
-	      breakpoint: 846,
-	      settings: {
-	      	slidesToShow: 2,
-     	}
-    },
-    {
-	      breakpoint: 585,
-	      settings: {
-	      	slidesToShow: 1,
-     	}
-    },
-  ]
-	});
-
 	$('.menu__btn').on('click', function(){
 		$('.menu__list').toggleClass('menu__list--active')
 	});
